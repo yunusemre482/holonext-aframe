5f5d1716cf57ec29e233a7d75c3de5e31be0e0b3
yunusemre482 <akgunyunus482@gmail.com>
2023-03-03 18:53:47 +0300

    remove router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,8 @@
 import React, { Suspense } from 'react';
 import Routes from './routes/Router';
 import { BrowserRouter as Router } from 'react-router-dom';
+import LocationBased from './pages/LocationBased';
+
 export default function App() {
-	return (
-		<Router>
-			<Suspense fallback={<div>Loading...</div>}>
-				<Routes />
-			</Suspense>
-		</Router>
-	);
+	return <LocationBased />;
 }
