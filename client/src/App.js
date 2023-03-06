520cf1680b4eee2a5513efd46f4ca04cb34b3d41
yunusemre482 <akgunyunus482@gmail.com>
2023-03-06 10:05:11 +0300

    add new chnages

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,4 @@
 import React, { Suspense } from 'react';
-import Routes from './routes/Router';
-import { BrowserRouter as Router } from 'react-router-dom';
 import LocationBased from './pages/LocationBased';
 
 export default function App() {
